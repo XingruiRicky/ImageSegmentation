549bb4eaf9ab56a1fa3f4036991d2b57d2e04da6
Xingrui <56009624+XingruiRicky@users.noreply.github.com>
2024-04-08 23:11:24 -0600

    finish segmentation

---
+++ dataPartition.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import random
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from IPython.display import Image, display
+from tensorflow.keras.preprocessing.image import load_img
+import PIL
+from PIL import ImageOps
+
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras import layers
+
